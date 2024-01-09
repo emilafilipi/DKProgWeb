980b283f1329014d056c45acf290d0bd8ca8e188
emilafilipi <emilafilipi30@gmail.com>
2024-01-09 21:13:12 +0100

    updates

--- gym-front/Finished/Services/script.js
+++ gym-front/Finished/Services/script.js
@@ -1,7 +1,43 @@
 function showDetails(id){
     var classId = document.getElementById(id);
-    if(classId == "strength"){
+    if(id === "strength"){
         var showClass = document.getElementById("showStrength");
         showClass.style.display="block";
+        document.getElementById("strength").style.display = "none";
+        document.getElementById("lift").style.display = "none";
+        document.getElementById("yoga").style.display = "none";
+        document.getElementById("cardio").style.display = "none";
     }
+    if(id === "lift"){
+        var showClass = document.getElementById("showLift");
+        showClass.style.display="block";
+        document.getElementById("strength").style.display = "none";
+        document.getElementById("lift").style.display = "none";
+        document.getElementById("yoga").style.display = "none";
+        document.getElementById("cardio").style.display = "none";
+    }
+    if(id === "yoga"){
+        var showClass = document.getElementById("showYoga");
+        showClass.style.display="block";
+        document.getElementById("strength").style.display = "none";
+        document.getElementById("lift").style.display = "none";
+        document.getElementById("yoga").style.display = "none";
+        document.getElementById("cardio").style.display = "none";
+    }
+    if(id === "cardio"){
+        var showClass = document.getElementById("showCardio");
+        showClass.style.display="block";
+        document.getElementById("strength").style.display = "none";
+        document.getElementById("lift").style.display = "none";
+        document.getElementById("yoga").style.display = "none";
+        document.getElementById("cardio").style.display = "none";
+    }
+}
+
+function removeDetails(){
+        document.getElementById("showStrength").style.display = "none";
+        document.getElementById("strength").style.display = "block";
+        document.getElementById("lift").style.display = "block";
+        document.getElementById("yoga").style.display = "block";
+        document.getElementById("cardio").style.display = "block";
 }
